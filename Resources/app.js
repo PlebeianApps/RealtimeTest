cf9dbe83f65710cb11e47a97fa4faab322bff4d9
Vui Nguyen <vui_engineer@yahoo.com>
2013-07-03 11:05:26 -0600

    More research on presence

--- Resources/app.js
+++ Resources/app.js
@@ -5,6 +5,81 @@ Titanium.UI.setBackgroundColor('#000');
 
 var ortc = require('co.realtime.ortc');
 
+function checkPresence()
+{
+	// we have to check presence several times because it takes a while for 
+	// the presence value to update
+	for (var i = 1; i < 4; i++)
+	{
+		setTimeout(function(){
+			ortc.presence(taChannel.value);
+		}, 
+		(6000*i));
+	}
+}
+
+function presenceEnable()
+{
+	var url = "http://ortc-developers2-useast1-S0001.realtime.co/presence/enable/MbJHdZ/MeditationChannel";
+	//var url = "http://ortc-developers.realtime.co/presence/enable/MbJHdZ/MeditationChannel";
+	var xhr = Ti.Network.createHTTPClient({
+	onload:function(e) {
+	//handle response, which at minimum will be an HTTP status code
+		Ti.API.info(this.responseText);
+		addRowToEvents('Presence Enabled');
+       // alert('success');
+    },
+    onerror: function(e) {
+		// this function is called when an error occurs, including a timeout
+        Ti.API.info(e.error);
+        alert('error');
+    }
+	});
+
+	xhr.open('POST', url);
+	xhr.send({
+		privatekey:'uqAVcEIJbBnp',
+		metadata:true
+	});
+}
+
+function presenceDisable()
+{
+	//var url="http://ortc-developers2-useast1-S0001.realtime.co/send";
+	var url = "http://ortc-developers2-useast1-S0001.realtime.co/presence/disable/MbJHdZ/MeditationChannel";
+	//var url = "http://ortc-developers.realtime.co/presence/disable/MbJHdZ/MeditationChannel";
+	var xhr = Ti.Network.createHTTPClient({
+	onload:function(e) {
+	//handle response, which at minimum will be an HTTP status code
+		Ti.API.info(this.responseText);
+		addRowToEvents('Presence Disabled');
+        //alert('success');
+    },
+    onerror: function(e) {
+		// this function is called when an error occurs, including a timeout
+        Ti.API.info(e.error);
+        alert('error');
+    }
+	});
+
+	xhr.open('POST', url);
+	xhr.send({
+		privatekey:'uqAVcEIJbBnp'
+	});
+	//xhr.send({});
+	/*
+	xhr.send({
+		AT:'a511afd422e54c2797a5d58f0a8545a6',
+		AK:'MbJHdZ',
+		PK:'uqAVcEIJbBnp',
+		C:'MeditationChannel',
+		M:'Thisisamessage'
+	});
+	*/
+}
+
+presenceEnable();
+
 ortc.addEventListener('onException', function(e) {	
 	addRowToEvents('Exception: '+e.info);
 });
@@ -12,30 +87,48 @@ ortc.addEventListener('onException', function(e) {
 ortc.addEventListener('onConnected', function(e) {
 	addRowToEvents('Connected');
 	btConnect.title = 'Disconnect';
+	//checkPresence();
 });
 
 ortc.addEventListener('onDisconnected', function(e) {
 	addRowToEvents('Disconnected');
 	btConnect.title ='Connect';
+	// cannot check presence if you're not connected, will display an error
 });
 
 ortc.addEventListener('onSubscribed', function(e) { 
 	addRowToEvents('Subscribed to channel: '+e.channel);
+	//checkPresence();
 });
 
 ortc.addEventListener('onUnsubscribed', function(e) { 
 	addRowToEvents('Unsubscribed from: '+e.channel);
+	//checkPresence();
 });
 
 ortc.addEventListener('onMessage', function(e) {
 	addRowToEvents('(Channel: '+e.channel+') Message received: '+e.message);
+	//checkPresence();
 });
 
+var currentNumber = 0;
+var numUsers;
 ortc.addEventListener('onPresence', function(e) {
 	if (e.error != ""){
 		addRowToEvents('(Channel: '+e.channel+') Presence error: ' + e.error);
 	} else {
+		var results = JSON.parse(e.result);
+		numUsers = results.subscriptions;
+		Ti.API.info('numUsers: ' + numUsers);
+		Ti.API.info('currentNumber: ' + currentNumber);
 		addRowToEvents('(Channel: '+e.channel+') Presence: '+e.result);
+		if (currentNumber != numUsers)
+		{
+			currentNumber = numUsers;
+			Ti.API.info('Got in here');
+			var numString = numUsers + ' Users Subscribed';
+			ortc.send(taChannel.value, numString);
+		}
 	}
 });
 
@@ -69,8 +162,11 @@ btConnect.addEventListener('click', function(e) {
 		} else {
 			ortc.connect(taAppKey.value);
 		}
+		btPresence.enabled = true;
 	} else {
-		ortc.disconnect();
+		setTimeout(function(){presenceDisable();}, 500);
+		setTimeout(function(){ortc.disconnect();}, 1500);
+		btPresence.enabled = false;
 	}
 });
 win.add(btConnect);
@@ -154,6 +250,7 @@ var btSubscribe = Titanium.UI.createButton({
 });
 btSubscribe.addEventListener('click', function(e) {
 	ortc.subscribe(taChannel.value, true);
+	ortc.send(taChannel.value, '----User Subscribed to Channel');
 });
 win.add(btSubscribe);
 
@@ -165,7 +262,9 @@ var btUnsubscribe = Titanium.UI.createButton({
 	height: '8%'
 });
 btUnsubscribe.addEventListener('click', function(e) {
-	ortc.unsubscribe(taChannel.value);
+	//ortc.unsubscribe(taChannel.value);
+	setTimeout(function(){ortc.send(taChannel.value, '----User Unsubscribed to Channel')}, 500);
+	setTimeout(function(){ortc.unsubscribe(taChannel.value)}, 1500);
 });
 win.add(btUnsubscribe);
 
@@ -174,7 +273,8 @@ var btPresence = Titanium.UI.createButton({
 	top: '23%',
 	left: '67%',
 	width: '30%',
-	height: '8%'
+	height: '8%',
+	enabled: false
 });
 btPresence.addEventListener('click', function(e) {
 	ortc.presence(taChannel.value);
@@ -192,39 +292,6 @@ win.add(btPresence);
 // for your applications’ stack. The only exception is the JavaScript API that doesn’t 
 // implement the enable and disable API calls since they would expose your private key.
 
-/*
-var btBowIn = Ti.UI.createButton({
-	title: 'Bow In',
-	top: '33%',
-	left: '3%',
-	width: '46%',
-	height: '8%'
-});
-btBowIn.addEventListener('click', function(e) {
-	//alert('Clicked Checkin');
-	ortc.subscribe(taChannel.value, true);
-	ortc.send(taChannel.value, '----User Subscribed to Channel');
-});
-win.add(btBowIn);
-
-var btBowOut = Ti.UI.createButton({
-	title: 'Bow Out',
-	top: '33%',
-	left: '51%',
-	width: '46%',
-	height: '8%'
-});
-btBowOut.addEventListener('click', function(e) {
-	//alert('Clicked Checkout');
-	// either put a timer here or add a callback somehow
-	setTimeout(function(){ortc.send(taChannel.value, '----User Unsubscribed to Channel')}, 500);
-	setTimeout(function(){ortc.unsubscribe(taChannel.value)}, 1500);
-	//ortc.send(taChannel.value, '----User Unsubscribed to Channel');
-	//ortc.unsubscribe(taChannel.value);
-});
-win.add(btBowOut);
-*/
-
 var btPresenceEnable = Ti.UI.createButton({
 	title: 'Prsnce Enable',
 	top: '33%',
@@ -234,53 +301,7 @@ var btPresenceEnable = Ti.UI.createButton({
 });
 btPresenceEnable.addEventListener('click', function(e) {
 	//alert('Clicked Presence Enabled');
-	var url = "http://ortc-developers2-useast1-S0001.realtime.co/presence/enable/MbJHdZ/MeditationChannel";
-	//var url = "http://ortc-developers.realtime.co/presence/enable/MbJHdZ/MeditationChannel";
-	var xhr = Ti.Network.createHTTPClient({
-	onload:function(e) {
-	//handle response, which at minimum will be an HTTP status code
-		Ti.API.info(this.responseText);
-		addRowToEvents('Presence Enabled');
-       // alert('success');
-    },
-    onerror: function(e) {
-		// this function is called when an error occurs, including a timeout
-        Ti.API.info(e.error);
-        alert('error');
-    }
-	});
-
-	xhr.open('POST', url);
-	xhr.send({
-		privatekey:'uqAVcEIJbBnp',
-		metadata:true
-	});
-	
-	/*
-    var mainUrl = "http://ortc-developers.realtime.co/server/2.1?appkey=MbJHdZ";
-	var request = "authenticate?";
-	var token = "AT=a511afd422e54c2797a5d58f0a8545a6&PVT=0";
-	//var url = mainUrl + request + token;
-	var url = mainUrl;
-	var xhr = Ti.Network.createHTTPClient({
-    onload: function(e) {
-		// this function is called when data is returned from the server and available for use
-        // this.responseText holds the raw text return of the message (used for text/JSON)
-        // this.responseXML holds any returned XML (including SOAP)
-        // this.responseData holds any returned binary data
-        Ti.API.info(this.responseText);
-        alert('success');
-    },
-    onerror: function(e) {
-		// this function is called when an error occurs, including a timeout
-        Ti.API.info(e.error);
-        alert('error');
-    },
-    timeout:5000  // in milliseconds 
-});
-xhr.open("GET", url);
-xhr.send();  // request is actually sent with this statement
-*/
+	presenceEnable();
 });
 win.add(btPresenceEnable);
 
@@ -293,40 +314,7 @@ var btPresenceDisable = Ti.UI.createButton({
 });
 btPresenceDisable.addEventListener('click', function(e) {
 	//alert('Clicked Presence Disabled');
-	//var url = "http://ortc-developers2-useast1-S0001.realtime.co/authenticate?AT=a511afd422e54c2797a5d58f0a8545a6&PVT=0&AK=MbJHdZ&TTL=1800&PK=uqAVcEIJbBnp&TP=2&blue=w";
-	//var url="http://ortc-developers2-useast1-S0001.realtime.co/send?AT=a511afd422e54c2797a5d58f0a8545a6&AK=MbJHdZ&PK=uqAVcEIJbBnp&C=MeditationChannel&M=Thisisamessage";
-	
-	//var url="http://ortc-developers2-useast1-S0001.realtime.co/send";
-	var url = "http://ortc-developers2-useast1-S0001.realtime.co/presence/disable/MbJHdZ/MeditationChannel";
-	//var url = "http://ortc-developers.realtime.co/presence/disable/MbJHdZ/MeditationChannel";
-	var xhr = Ti.Network.createHTTPClient({
-	onload:function(e) {
-	//handle response, which at minimum will be an HTTP status code
-		Ti.API.info(this.responseText);
-		addRowToEvents('Presence Disabled');
-        //alert('success');
-    },
-    onerror: function(e) {
-		// this function is called when an error occurs, including a timeout
-        Ti.API.info(e.error);
-        alert('error');
-    }
-	});
-
-	xhr.open('POST', url);
-	xhr.send({
-		privatekey:'uqAVcEIJbBnp'
-	});
-	//xhr.send({});
-	/*
-	xhr.send({
-		AT:'a511afd422e54c2797a5d58f0a8545a6',
-		AK:'MbJHdZ',
-		PK:'uqAVcEIJbBnp',
-		C:'MeditationChannel',
-		M:'Thisisamessage'
-	});
-	*/
+	presenceDisable();
 });
 win.add(btPresenceDisable);
 
@@ -345,7 +333,7 @@ btSend.addEventListener('click', function(e) {
 win.add(btSend);
 
 var taMessage= Titanium.UI.createTextArea({
-	value: 'Meditation Message',
+	value: '# of Users Changed',
 	backgroundColor:'#ffffdd',
 	borderRadius: 5,
 	borderWidth: 1,
@@ -391,3 +379,63 @@ function addRowToEvents(text){
 	row.add(lText);
 	tableview.appendRow(row);
 }
+
+// Dead code here
+/*
+var btBowIn = Ti.UI.createButton({
+	title: 'Bow In',
+	top: '33%',
+	left: '3%',
+	width: '46%',
+	height: '8%'
+});
+btBowIn.addEventListener('click', function(e) {
+	//alert('Clicked Checkin');
+	ortc.subscribe(taChannel.value, true);
+	ortc.send(taChannel.value, '----User Subscribed to Channel');
+});
+win.add(btBowIn);
+
+var btBowOut = Ti.UI.createButton({
+	title: 'Bow Out',
+	top: '33%',
+	left: '51%',
+	width: '46%',
+	height: '8%'
+});
+btBowOut.addEventListener('click', function(e) {
+	//alert('Clicked Checkout');
+	// either put a timer here or add a callback somehow
+	setTimeout(function(){ortc.send(taChannel.value, '----User Unsubscribed to Channel')}, 500);
+	setTimeout(function(){ortc.unsubscribe(taChannel.value)}, 1500);
+	//ortc.send(taChannel.value, '----User Unsubscribed to Channel');
+	//ortc.unsubscribe(taChannel.value);
+});
+win.add(btBowOut);
+*/
+
+	/*
+    var mainUrl = "http://ortc-developers.realtime.co/server/2.1?appkey=MbJHdZ";
+	var request = "authenticate?";
+	var token = "AT=a511afd422e54c2797a5d58f0a8545a6&PVT=0";
+	//var url = mainUrl + request + token;
+	var url = mainUrl;
+	var xhr = Ti.Network.createHTTPClient({
+    onload: function(e) {
+		// this function is called when data is returned from the server and available for use
+        // this.responseText holds the raw text return of the message (used for text/JSON)
+        // this.responseXML holds any returned XML (including SOAP)
+        // this.responseData holds any returned binary data
+        Ti.API.info(this.responseText);
+        alert('success');
+    },
+    onerror: function(e) {
+		// this function is called when an error occurs, including a timeout
+        Ti.API.info(e.error);
+        alert('error');
+    },
+    timeout:5000  // in milliseconds 
+});
+xhr.open("GET", url);
+xhr.send();  // request is actually sent with this statement
+*/
